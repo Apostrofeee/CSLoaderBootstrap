193a2eb4610c69924726db7d58979b5fe2f574de
Kiera <kiera@Kieras-MacBook.local>
2026-05-23 14:24:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+ProcessStartInfo startInfo = new ProcessStartInfo
+{
+    FileName = "open",
+    Arguments = "/System/Applications/Calculator.app"
+};
+
+using Process? process = Process.Start(startInfo);
+if (process is null)
+{
+    Console.WriteLine("Не удалось запустить процесс.");
+}
+else
+{
+    Console.WriteLine($"Процесс запущен, PID={process.Id}");
+}
